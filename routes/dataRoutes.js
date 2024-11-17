647dd398d21d1e118788a4bd0f2fd13a4849256e
Sarvenaz Sinaei <sarvenaz.sinaei@student.nbi-handelsakademin.se>
2024-11-17 22:37:35 +0100

    Added error handling middleware and logging with Morgan and Winston

--- routes/dataRoutes.js
+++ routes/dataRoutes.js
@@ -1,22 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const VehicleData = require('../models/VehicleData');
+const logger = require('../utils/logger');
 
 let vehicleData = [
     { id: 1, speed: 80, fuelConsumption: 5.2, safetyWarnings: 1 }
 ];
 
-router.get('/data', async(req, res) => {
+router.get('/data', async(req, res, next) => {
     try {
         const data = await VehicleData.find();
         res.json(data);
     }
     catch (error) {
-        res.status(500).json({ error: 'Failed to retrieve data'});
+        logger.error(`Error fetching data: ${error.message}`);
+        next(error);
     }
 });
 
-router.post('/data', async(req, res) => {
+router.post('/data', async(req, res, next) => {
     const { speed, fuelConsumption, safetyWarnings } = req.body;
     try {
         const newData = new VehicleData({ speed, fuelConsumption, safetyWarnings });
@@ -24,7 +26,7 @@ router.post('/data', async(req, res) => {
         res.status(201).json(newData);
     }
     catch (error) {
-        res.status(500).json({ error: 'Failed to save data' });
+        next(error);
     }
 });
 
--- server.js
+++ server.js
@@ -3,8 +3,11 @@ const http = require('http');
 const { Server } = require('socket.io');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const morgan = require('morgan');
+const logger = require('./utils/logger');
 
 const dataRoutes = require('./routes/dataRoutes');
+const { error } = require('console');
 
 const app = express();
 const server = http.createServer(app); 
@@ -12,6 +15,15 @@ const io = new Server(server);
 
 app.use(cors({ origin: 'http://localhost:3001' }));
 app.use(express.json());
+app.use(morgan('combined'));
+app.use((err, req, res, next) => {
+    logger.error(`${err.message} - ${req.method} ${req.originalUrl}`);
+    res.status(err.status || 500).json({
+        error: {
+            message: err.message || 'Internal Server Error'
+        }
+    });
+});
 
 const mongoURI = 'mongodb://localhost:27017/vehicleDataDB';
 
@@ -45,6 +57,21 @@ io.on('connection', (socket) => {
         console.log('Client disconnected');
     });
 });
+
+//Error handling middleware
+app.use((err, req, res, next) => {
+    console.error('Error:', err.message);
+    res.status(err.status || 500).json({
+        error: {
+            message: err.message || 'Internal Server Error'
+        }
+    });
+});
+
+//Testloggor
+logger.info('This is an info log');
+logger.warn('This is a warning log');
+logger.error('This is an error log');
 
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
